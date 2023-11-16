269f03a28c2aa47618a6ce5a785d720c7c5e8082
jemand2001 <>
2023-11-16 23:45:30 +0100

    countdowns now say "today" the day they're due

--- app/XMonad/Actions/Countdown.hs
+++ app/XMonad/Actions/Countdown.hs
@@ -1,19 +1,16 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+
 module XMonad.Actions.Countdown (notifyCountdowns) where
 
-import System.IO
-
-import XMonad.Core
-import Data.Time.Calendar
-import Data.Time
-
-import XMonad.Actions.StatefulNotify
-import Data.List.Split (splitOn)
 import Data.Char (isSpace)
-
-import XMonad.Util.Time
-
+import Data.List.Split (splitOn)
+import Data.Time
+import Data.Time.Calendar
+import System.IO
+import XMonad.Actions.StatefulNotify
+import XMonad.Core
 import XMonad.Util.Paths
+import XMonad.Util.Time
 
 type Countdown = (String, Day)
 
@@ -23,11 +20,13 @@ notifyCountdowns p = do
   replaceStateNotification "countdowns" "Countdowns" $ do
     countdowns <- io $ getCountdowns path
     today <- localDay <$> now
-    return $ unlines [
-        title ++ ": " ++ show (diffDays day today) ++ " days" |
-        (title, day) <- countdowns
-      , day >= today
-      ]
+    return $
+      unlines $
+        [title ++ ": today!" | (title, day) <- countdowns, day == today]
+          ++ [ title ++ ": " ++ show (diffDays day today) ++ " days"
+               | (title, day) <- countdowns,
+                 day > today
+             ]
 
 getCountdowns :: FilePath -> IO [Countdown]
 getCountdowns p = do
